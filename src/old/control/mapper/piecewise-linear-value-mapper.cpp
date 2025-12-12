ab87271c178854156f4df0ad087cc64271ee3b36
wibot <zhoujian.ww@gmail.com>
2025-12-12 17:21:28 +0800

    删除无用的历史文件

--- src/old/control/mapper/piecewise-linear-value-mapper.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by zhouj on 2022/12/10.
-//
-
-#include "piecewise-linear-value-mapper.hpp"
-
-namespace wibot {}  // namespace wibot
--- src/old/control/mapper/value-mapper.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by zhouj on 2022/11/21.
-//
-
-#include "value-mapper.hpp"
-
-namespace wibot {}  // namespace wibot
